92ad1b1c33488deae2cd2e210b81147f24a7fef0
Roy <64872041+maxkuangsy@users.noreply.github.com>
2022-11-17 23:19:56 -0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === '../images/dog.png') {
+      myImage.setAttribute('src', '../images/school_badge.png');
+    } else {
+      myImage.setAttribute('src', '../images/dog.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myname){
+  setUserName();
+  }
+  else{
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'A lovely dog，' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+
+myButton.onclick = function() {
+   setUserName();
+}
